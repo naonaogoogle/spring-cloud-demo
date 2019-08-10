56d90c7247baad8e6d1d35b6a676844eb05ec45a
huanghaodong <1203891770hhd@gmail.com>
2019-08-10 19:10:57 +0800

    1.Ribbon 中使用熔断器

--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/naonao/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/naonao/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -3,6 +3,7 @@ package com.naonao.hello.spring.cloud.web.admin.ribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @Description:
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class WebAdminRibbonApplication {
     public static void main(String[] args) {
 
--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/naonao/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/naonao/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.naonao.hello.spring.cloud.web.admin.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,8 +18,13 @@ public class AdminService {
         this.restTemplate = restTemplate;
     }
 
+    @HystrixCommand(fallbackMethod = "sayHiError")
     public String sayHi(String message) {
         return restTemplate.getForObject("http://HELLO-SPRING-CLOUD-SERVICE-ADMIN/hi?message=" + message, String.class);
+    }
+
+    public String sayHiError(String message) {
+        return String.format("Hi, your message is : %s, but request error.", message);
     }
 
 }

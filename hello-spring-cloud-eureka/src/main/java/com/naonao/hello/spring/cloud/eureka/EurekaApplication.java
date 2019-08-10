09d2030c7ffcf9cde90ab48737ad800a87a2b69b
huanghaodong <1203891770hhd@gmail.com>
2019-08-10 14:10:44 +0800

    1.服务注册与发现.

---
+++ hello-spring-cloud-eureka/src/main/java/com/naonao/hello/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.naonao.hello.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @Author: HuangHaodong
+ * @Date: 2019-08-10 14:02
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+
+    }
+}
